8477b44d499df898f8b4c49398909b9b34f40729
Lucas Licar <lucas.licar2016@gmail.com>
2020-10-03 23:05:56 -0300

    upload de arquivos para o S3 - incompleto

--- tabela.php
+++ tabela.php
@@ -2,7 +2,7 @@
 include("conexao.php");
 
 function tabela(){
-    
+    $s3_URL = 'https://aedha-projeto.s3.amazonaws.com';
     $sql="SELECT * FROM ocorrencias order by nome";
     $query=mysqli_query(conexao(),$sql);
     while($vetor=mysqli_fetch_array($query)){
@@ -15,8 +15,8 @@ function tabela(){
                 <td> $data</td>
                 <td> Rua: $vetor[rua],$vetor[numero]</td>
                 <td> $vetor[bairro]</td>
-                <td> <a href='imagens/locais/$vetor[imagem_local]' target='_blank' > Abrir Imagem </td>
-                <td> <a href='imagens/mapas/$vetor[imagem_mapa]' target='_blank' > Abrir Imagem </td>
+                <td> <a href='$s3_URL/locais/$vetor[imagem_local]' target='_blank' > Abrir Imagem </td>
+                <td> <a href='$s3_URL/mapas/$vetor[imagem_mapa]' target='_blank' > Abrir Imagem </td>
                 <td> $vetor[descricao] </td>
             </tr>
          ";
--- upload_arquivos.php
+++ upload_arquivos.php
@@ -1,11 +1,32 @@
 <?php 
+require 'vendor/autoload.php';
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
 
-function upload($pasta,$file)
+function upload($file,$local)
 {
+    
     $name = md5($file["name"]);
     $tipo = strrchr($file["name"],".");
+    $tmp_file_name = "{$name}.{$tipo}";
     // Move o arquivo da pasta temporaria de upload para a pasta de destino 
-    move_uploaded_file($file["tmp_name"], "$pasta/".$name.$tipo);
+
+    $clientS3 = S3Client::factory(array(
+        'key'    => "AKIAI4C2TTWPOGZKQSUQ",
+        'secret' => "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"
+    ));
+    
+    try{
+
+        $clientS3 ->putObject([
+            'Bucket' => "aedha-projeto/$local",
+            'Key'    => $tmp_file_name,
+            'SourceFile' =>$file['tmp_name'],
+        ]);
+
+    } catch(S3Exeption $e) {
+        die("Não foi possivel enviar o arquivo");
+    }
     
       
 
